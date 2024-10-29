f278b088b8689a75c75a52969d55de825b0a0296
s187586 <s187586@student.pg.edu.pl>
2024-10-29 13:15:43 +0100

    feat: add extensions method

--- PetShop/EnumerableTools.cs
+++ PetShop/EnumerableTools.cs
@@ -3,7 +3,10 @@ using Training.DomainClasses;
 
 public static class EnumerableTools
 {
-    public static IEnumerable<TItem> OneAtTime<TItem>(IEnumerable<TItem> items)
+    //ctrl+shift+r = refactor
+    //ctrl+shift+g = navigate to
+    //extension only for public class&method and method static
+    public static IEnumerable<TItem> OneAtTime<TItem>(this IEnumerable<TItem> items)
     {
         foreach (var item in items)
         {
