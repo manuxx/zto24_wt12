22a1c790ef8681219067673975ab46a84e494bfb
Kambion <97839446+Kambion@users.noreply.github.com>
2024-10-29 13:22:42 +0100

    zad1

---
+++ PetShop/ReadOnlySet.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Training.DomainClasses
+{
+    public class ReadOnlySet<TItem> : IEnumerable<TItem>
+    {
+        private readonly IEnumerable<TItem> _pets;
+
+        public ReadOnlySet(IEnumerable<TItem> pets)
+        {
+            _pets = pets;
+            //throw new NotImplementedException();
+        }
+
+        public IEnumerator<TItem> GetEnumerator()
+        {
+            return _pets.GetEnumerator();
+            //throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
