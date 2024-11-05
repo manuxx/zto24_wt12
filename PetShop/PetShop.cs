4d0fb837a5900f9cf887a667dc87c6bcec35e5c0
kosskas <93159483+kosskas@users.noreply.github.com>
2024-11-05 12:52:08 +0100

    Add queries

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -29,11 +29,7 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllCats()
         {
-            foreach (Pet pet in _petsInTheStore)
-            {
-                if(pet.species==Species.Cat)
-                    yield return pet;
-            }
+            return AllThat(pet => pet.species == Species.Cat);
         }
 
         public IEnumerable<Pet> AllPetsSortedByName()
@@ -41,6 +37,55 @@ namespace Training.DomainClasses
             var ret = new List<Pet>(_petsInTheStore);
             ret.Sort((p1,p2) => p1.name.CompareTo(p2.name));
             return ret;
+        }
+
+        public IEnumerable<Pet> AllMice()
+        {
+            return AllThat(pet => pet.species == Species.Mouse);
+        }
+
+        public IEnumerable<Pet> AllFemalePets()
+        {
+            return AllThat(pet => pet.sex == Sex.Female);
+        }
+
+        public IEnumerable<Pet> AllCatsOrDogs()
+        {
+            return AllThat(pet => pet.species == Species.Cat || pet.species == Species.Dog);
+        }
+
+        public IEnumerable<Pet> AllPetsButNotMice()
+        {
+            return AllThat(pet => pet.species != Species.Mouse);
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2010()
+        {
+            return AllThat(pet => pet.yearOfBirth > 2010);
+        }
+
+        public IEnumerable<Pet> AllDogsBornAfter2010()
+        {
+            return AllThat(pet => pet.yearOfBirth > 2010 && pet.species == Species.Dog);
+        }
+
+        public IEnumerable<Pet> AllMaleDogs()
+        {
+            return AllThat(pet => pet.species == Species.Dog && pet.sex == Sex.Male);
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
+        {
+            return AllThat(pet => pet.yearOfBirth > 2011 || pet.species == Species.Rabbit);
+        }
+
+        public IEnumerable<Pet> AllThat(Func<Pet, bool> condition)
+        {
+            foreach (Pet pet in _petsInTheStore)
+            {
+                if (condition(pet))
+                    yield return pet;
+            }
         }
     }
 }
