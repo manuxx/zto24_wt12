d8d053c53aac3da7ed7455fd7e7cffe85b13238e
kosskas <93159483+kosskas@users.noreply.github.com>
2024-11-05 14:08:34 +0100

    Add Criteria class

--- PetShop/EnumerableTools.cs
+++ PetShop/EnumerableTools.cs
@@ -12,12 +12,36 @@ public static class EnumerableTools
         }
     }
 
-    public static IEnumerable<TItem> AllThat<TItem>(this IEnumerable<TItem> items, Predicate<TItem> condition)
+    public static IEnumerable<Pet> AllThat<Pet>(this IEnumerable<Pet> items, Predicate<Pet> condition)
+    {
+        return items.AllThat(new AnonymousCriteria<Pet>(condition));
+    }
+    public static IEnumerable<TItem> AllThat<TItem>(this IEnumerable<TItem> items, Criteria<TItem> criteria)
     {
         foreach (TItem item in items)
         {
-            if (condition(item))
+            if (criteria.IsSatisfiedBy(item))
                 yield return item;
         }
     }
+}
+
+public class AnonymousCriteria<TItem> : Criteria<TItem>
+{
+    private readonly Predicate<TItem> _condition;
+
+    public AnonymousCriteria(Predicate<TItem> condition)
+    {
+        _condition = condition;
+    }
+
+    public bool IsSatisfiedBy(TItem item)
+    {
+        return _condition(item);
+    }
+}
+
+public interface Criteria<TItem>
+{
+    public bool IsSatisfiedBy(TItem item);
 }
