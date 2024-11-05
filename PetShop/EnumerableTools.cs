6fcaa0bed08dcd84ad7506be9ed34eae5bd2df86
archaikek <f.szczepanski@op.pl>
2024-11-05 14:06:11 +0100

    Przerobienie metody AllThat na klasę Criteria

--- PetShop/EnumerableTools.cs
+++ PetShop/EnumerableTools.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Microsoft.Win32.SafeHandles;
 using Training.DomainClasses;
 
 public static class EnumerableTools
@@ -12,11 +13,35 @@ public static class EnumerableTools
         }
     }
 
-    public static IEnumerable<TItem> AllThat<TItem>(this IEnumerable<TItem> items, Func<TItem, bool> condition)
+    public static IEnumerable<Pet> AllThat(this IEnumerable<Pet> pets, Func<Pet, bool> condition)
     {
-        foreach (TItem item in items)
+        return pets.AllThat(new AnonymousCriteria<Pet>(condition));
+    }
+
+    public static IEnumerable<Pet> AllThat(this IEnumerable<Pet> pets, Critertia<Pet> critertia)
+    {
+        foreach (Pet pet in pets)
         {
-            if (condition(item)) yield return item;
+            if (critertia.IsSatisfiedBy(pet)) yield return pet;
         }
     }
+}
+
+public class AnonymousCriteria<T> : Critertia<T>
+{
+    private Func<T, bool> _condition;
+    public AnonymousCriteria(Func<T, bool> condition)
+    {
+        _condition = condition;
+    }
+
+    public bool IsSatisfiedBy(T item)
+    {
+        return _condition(item);
+    }
+}
+
+public interface Critertia<T>
+{
+    bool IsSatisfiedBy(T item);
 }
