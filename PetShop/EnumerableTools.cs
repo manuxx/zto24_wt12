12a25d79f7ba7e23f509ceeda23811246d5cfcaf
parol.szymon <parol.szymon@protonmail.com>
2024-11-05 14:12:22 +0100

    AnonymousCriteria added and working

--- PetShop/EnumerableTools.cs
+++ PetShop/EnumerableTools.cs
@@ -5,14 +5,41 @@ namespace Training.DomainClasses;
 
 public static class EnumerableTools
 {
+
     public static IEnumerable<Pet> AllThat(this IList<Pet> petsInTheStore, Func<Pet, bool> condition)
+    {
+        return petsInTheStore.AllThat(new AnonymousCriteria<Pet>(condition));
+    }
+
+    public static IEnumerable<Pet> AllThat(this IList<Pet> petsInTheStore, Criteria<Pet> criteria)
     {
         foreach (var pet in petsInTheStore)
         {
-            if (condition(pet))
+            if (criteria.IsSatisfiedBy(pet))
             {
                 yield return pet;
             }
         }
     }
+}
+
+public class AnonymousCriteria<T> : Criteria<T>
+{
+    private Func<Pet, bool> _condition;
+
+    public AnonymousCriteria(Func<Pet, bool> condition)
+    {
+        _condition = condition;
+    }
+
+    public bool IsSatisfiedBy(Pet pet)
+    {
+        if(_condition(pet)) return true;
+        return false;
+    }
+}
+
+public interface Criteria<T>
+{
+    bool IsSatisfiedBy(Pet pet);
 }
