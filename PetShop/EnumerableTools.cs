db7f77f80cb30fcb02ff97021d0fa681ad645227
s187586 <s187586@student.pg.edu.pl>
2024-11-05 14:10:56 +0100

    feat: AnonymousCriteria class with Criteria interface

--- PetShop/EnumerableTools.cs
+++ PetShop/EnumerableTools.cs
@@ -12,12 +12,37 @@ public static class EnumerableTools
         }
     }
 
-    public static IEnumerable<TItem> AllThat<TItem>(this IEnumerable<TItem> petsInTheStore, Predicate<TItem> condition)
+    public static IEnumerable<Pet> AllThat<Pet>(this IEnumerable<Pet> petsInTheStore, Predicate<Pet> condition)
     {
-        foreach (TItem pet in petsInTheStore)
+        return petsInTheStore.AllThat(new AnonymousCriteria<Pet>(condition));
+    }
+
+    public static IEnumerable<Pet> AllThat<Pet>(this IEnumerable<Pet> petsInTheStore, Criteria<Pet> criteria)
+    {
+        foreach (Pet pet in petsInTheStore)
         {
-            if (condition(pet))
+            if (criteria.isSatisfiedBy(pet))
                 yield return pet;
         }
     }
+}
+
+public class AnonymousCriteria<T> : Criteria<T>
+{
+    private Predicate<T> _condition;
+
+    public AnonymousCriteria(Predicate<T> condition)
+    {
+        _condition = condition;
+    }
+
+    public bool isSatisfiedBy(T pet)
+    {
+        return _condition(pet);
+    }
+}
+
+public interface Criteria<T>
+{
+    bool isSatisfiedBy(T pet);
 }
