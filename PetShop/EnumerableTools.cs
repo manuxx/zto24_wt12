4b93eda2341b2d0cfb6e124499e0aa7095c54f8a
Jakub Bednarz <kubabednarz1709@gmail.com>
2024-11-05 13:07:28 +0100

    refactor to extension method in utility class

--- PetShop/EnumerableTools.cs
+++ PetShop/EnumerableTools.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Training.DomainClasses;
 
@@ -8,6 +9,17 @@ public static class EnumerableTools
         foreach (var item in items)
         {
             yield return item;
+        }
+    }
+
+    public static IEnumerable<Pet> AllThat(this IEnumerable<Pet> petsInTheStore, Func<Pet, bool> condition)
+    {
+        foreach (var pet in petsInTheStore)
+        {
+            if (condition(pet))
+            {
+                yield return pet;
+            }
         }
     }
 }
