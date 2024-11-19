696afe3b99768dfc4d9f0fcfbc7766caca24f15b
Tomek5011 <gdyniatomek@gmail.com>
2024-11-19 14:03:40 +0100

    zrobione wszystko

--- PetShop/EnumerableTools.cs
+++ PetShop/EnumerableTools.cs
@@ -5,10 +5,10 @@ public static class EnumerableTools
 {
 	public static IEnumerable<TItem> AllThat<TItem>(this IEnumerable<TItem> items, Criteria<TItem> criteria)
     {
-        foreach (TItem pet in items)
+        foreach (TItem item in items)
         {
-            if (criteria.IsSatisfiedBy(pet))
-                yield return pet;
+            if (criteria.IsSatisfiedBy(item))
+                yield return item;
         }
     }
 }
